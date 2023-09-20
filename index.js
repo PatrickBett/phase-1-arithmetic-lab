1e10b1ecba0092e452f84525f05681ebd387c9a4
PatrickBett <patrickbett018@gmail.com>
2023-09-20 05:30:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+
+var multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24;
+let num4 = 5;
+
+let mod = num3 % num4;
+let max = Math.max(1,3,4,5,6,7,6,8,14,20)
